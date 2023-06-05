753cec22bb6047cd716ac482854a2803b24ee311
Nguyễn Quang Tính <100455964+tinhpx2911@users.noreply.github.com>
2023-06-06 01:32:38 +0700

    Update train_ddp.py

--- train_ddp.py
+++ train_ddp.py
@@ -28,7 +28,7 @@ class Trainer:
             output_dir: str = 'checkpoints/',  
             num_epochs: int = 10, 
             max_length: int = 128, 
-            batch_size: int = 2,
+            batch_size: int = 4,
             mixed_precision_dtype =  None,
             gradient_accumulation_steps: int = 16):
         """
@@ -347,7 +347,7 @@ if __name__ == "__main__":
         max_length = max_length,
         batch_size = batch_size,
         gpu_id=local_rank,
-        mixed_precision_dtype = torch.float16,  #TODO: Set the mixed precision data type, hint use float16
+        mixed_precision_dtype = None, #torch.float16,  #TODO: Set the mixed precision data type, hint use float16
         tokenizer=tokenizer,
         output_dir= OUTPUT_DIR,
         is_ddp_training = True if distributed_strategy == "ddp" else False,
